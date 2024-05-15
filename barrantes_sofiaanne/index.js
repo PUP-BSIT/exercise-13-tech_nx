9a58ab0fa79a44de3d3eb0b1915f5f9fc370200b
Sofia <sabarrantes2911@gmail.com>
2024-05-15 22:29:55 +0800

    add changes

--- barrantes_sofiaanne/index.js
+++ barrantes_sofiaanne/index.js
@@ -3,7 +3,7 @@ function comment_btn() {
     let comment = document.getElementById("comment");
     let button = document.getElementById("com-b");
 
-    if (name.value.length > 0 && comment.value.length > 0) {
+    if (name.value.length && comment.value.length) {
         button.disabled = false;
     } else {
         button.disabled = true;

c18348bd4c35fb268dc2062b18324f8ba37bfcbc
alona pepito <pepitoalona231@gmail.com>
2024-05-15 21:21:19 +0800

    add files

---
+++ pepito_alonajane/script.js
@@ -0,0 +1,16 @@
+document.getElementById("name").addEventListener
+        ("input", toggleCommentButton);
+document.getElementById("comment").addEventListener
+        ("input", toggleCommentButton);
+
+function toggleCommentButton() {
+  let nameInput = document.getElementById("name").value.trim();
+  let commentInput = document.getElementById("comment").value.trim();
+  let commentBtn = document.getElementById("comment-btn");
+
+  if (nameInput && commentInput) {
+    commentBtn.removeAttribute("disabled");
+  } else {
+    commentBtn.setAttribute("disabled", "disabled");
+  }
+}
